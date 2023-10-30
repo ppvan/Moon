3cdffce94ba7357794dbbc34fa4f6bdb350f70ed
ppvan <phuclaplace@gmail.com>
2023-10-30 11:29:33 +0700

    chore: setup DI container

---
+++ moon-fe/app/src/main/java/me/ppvan/moon/MoonApplication.kt
@@ -0,0 +1,9 @@
+package me.ppvan.moon
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MoonApplication : Application() {
+
+}
--- moon-fe/app/src/main/java/me/ppvan/moon/ui/screens/activity/MainActivity.kt
+++ moon-fe/app/src/main/java/me/ppvan/moon/ui/screens/activity/MainActivity.kt
@@ -1,17 +1,26 @@
-package me.ppvan.moon
+package me.ppvan.moon.ui.screens.activity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import me.ppvan.moon.ui.player.Player
 import me.ppvan.moon.ui.theme.MoonTheme
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +31,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MoonApp()
                 }
             }
         }
+    }
+}
+
+
+@Composable
+fun MoonApp(mainViewModel: MainViewModel = hiltViewModel()) {
+
+    Column (verticalArrangement = Arrangement.Center, modifier = Modifier.padding(20.dp)) {
+        Greeting(mainViewModel.message)
+        Player()
     }
 }
 
@@ -35,12 +54,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MoonTheme {
-        Greeting("Android")
+        MoonApp()
     }
 }
