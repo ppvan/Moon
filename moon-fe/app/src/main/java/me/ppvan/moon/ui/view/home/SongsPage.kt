0d021d1f5285f54fe482ddfd320c3de412312385
ppvan <phuclaplace@gmail.com>
2023-11-21 12:05:41 +0700

    fix: fix lazy column not recompose

--- moon-fe/app/src/main/java/me/ppvan/moon/ui/view/home/SongsPage.kt
+++ moon-fe/app/src/main/java/me/ppvan/moon/ui/view/home/SongsPage.kt
@@ -1,13 +1,11 @@
 package me.ppvan.moon.ui.view.home
 
 import android.content.Intent
-import android.net.Uri
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,17 +17,9 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.PlaylistAdd
-import androidx.compose.material.icons.automirrored.filled.PlaylistPlay
-import androidx.compose.material.icons.filled.Album
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.outlined.EditNote
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.outlined.EditNote
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
@@ -38,7 +28,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -52,7 +41,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.core.content.ContextCompat.startActivity
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import me.ppvan.moon.R
@@ -66,13 +54,18 @@ import me.ppvan.moon.ui.viewmodel.MoonPlayer
 fun SongsPage(context: ViewContext) {
 
     val trackViewModel = context.trackViewModel
-    val allTracks by trackViewModel.allTracks.collectAsState()
+    val allTracks = trackViewModel.allTracks.toList()
     val player: MoonPlayer = trackViewModel.player
 
     Surface(
         modifier = Modifier
             .fillMaxSize(), color = MaterialTheme.colorScheme.surface
     ) {
+//        Column {
+//            for (track in allTracks) {
+//                Text(text = track.title)
+//            }
+//        }
         SongList(songs = allTracks, navigator = context.navigator) {
             player.load(allTracks)
             player.preparePlay(it)
@@ -87,16 +80,20 @@ fun SongList(
     songs: List<Track> = List(10) { Track.DEFAULT },
     onItemClick: (item: Track) -> Unit = {},
 ) {
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(4.dp)
-    ) {
-        items(songs) {song ->
-            SongListItem(
-                Modifier.fillMaxWidth(),
-                track = song,
-                onClick = { onItemClick(song) },
-                navigator = navigator
-            )
+    Column {
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            items(songs, key = { track ->
+                track.contentUri
+            }) {song ->
+                SongListItem(
+                    Modifier.fillMaxWidth(),
+                    track = song,
+                    onClick = { onItemClick(song) },
+                    navigator = navigator
+                )
+            }
         }
     }
 }
