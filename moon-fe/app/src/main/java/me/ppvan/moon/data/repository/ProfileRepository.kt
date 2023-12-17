544ffcf0ec28731adfeb87241406d8013fc336ca
ppvan <phuclaplace@gmail.com>
2023-12-17 23:24:49 +0700

    feat: profile logic

--- moon-fe/app/src/main/java/me/ppvan/moon/data/repository/ProfileRepository.kt
+++ moon-fe/app/src/main/java/me/ppvan/moon/data/repository/ProfileRepository.kt
@@ -15,16 +15,6 @@ class ProfileRepository @Inject constructor() {
         _loggedInUser.value = User.default()
     }
 
-    // Cập nhật tên người dùng
-    fun updateName(newName: String) {
-        val currentUser = _loggedInUser.value
-        _loggedInUser.value = currentUser.copy(name = newName)
-    }
 
-    // Cập nhật email người dùng
-    fun updateEmail(newEmail: String) {
-        val currentUser = _loggedInUser.value
-        _loggedInUser.value = currentUser.copy(email = newEmail)
-    }
 
 }
