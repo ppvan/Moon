7a787228845d7ae4b58a692a296aeebee8453500
ppvan <phuclaplace@gmail.com>
2023-12-17 23:24:49 +0700

    feat: profile logic

--- moon-fe/app/src/main/java/me/ppvan/moon/ui/view/home/ProfilePage.kt
+++ moon-fe/app/src/main/java/me/ppvan/moon/ui/view/home/ProfilePage.kt
@@ -1,8 +1,11 @@
 package me.ppvan.moon.ui.view.home
 
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,46 +14,58 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import me.ppvan.moon.R
 import me.ppvan.moon.ui.activity.ViewContext
 import me.ppvan.moon.ui.component.EditEmailDialog
 import me.ppvan.moon.ui.component.EditNameDialog
 import me.ppvan.moon.ui.component.OptionRow
-import me.ppvan.moon.ui.component.UpdateSuccessfulDialog
-import me.ppvan.moon.ui.component.UserImage
 
 @Composable
 fun ProfilePage(context: ViewContext) {
     val profileViewModel = context.profileViewModel
-    val user by profileViewModel.loggedInUser
-    var name by remember { mutableStateOf(user.name) }
-    var email by remember { mutableStateOf(user.email) }
-    var isEditingName by remember { mutableStateOf(false) }
-    var isEditingEmail by remember { mutableStateOf(false) }
-    var newName by remember { mutableStateOf(name) }
-    var newEmail by remember { mutableStateOf(email) }
-    var isUpdateSuccessfulDialogOpen by remember { mutableStateOf(false) }
+    val photoPicker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia(),
+        onResult = { uri ->
+            profileViewModel.onAvatarUrlChange(uri.toString())
+        }
+    )
+
+    val firstName by profileViewModel.firstName.collectAsState()
+    val lastName by profileViewModel.lastName.collectAsState()
+    val avatarUrl by profileViewModel.avatarUrl.collectAsState()
+
+    val editFirstName by profileViewModel.editFirstName.collectAsState()
+    val editLastName by profileViewModel.editLastName.collectAsState()
+    val currentContext = LocalContext.current
+
     var isChangesMade by remember { mutableStateOf(false) }
+
     Surface(
         modifier = Modifier
             .fillMaxSize(), color = MaterialTheme.colorScheme.surface
-    ){
+    ) {
 
         Column(
             modifier = Modifier
@@ -58,7 +73,16 @@ fun ProfilePage(context: ViewContext) {
                 .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            UserImage(R.drawable.bocchi)
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(avatarUrl)
+                    .error(R.drawable.bocchi).build(),
+                contentScale = ContentScale.Crop,
+                contentDescription = "User Avatar",
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(250.dp),
+            )
 
             Spacer(modifier = Modifier.height(8.dp))
 
@@ -66,6 +90,11 @@ fun ProfilePage(context: ViewContext) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .clickable {
+                        photoPicker.launch(
+                            PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                        )
+
+                        isChangesMade = true
                     }
                     .padding(horizontal = 16.dp),
                 verticalAlignment = Alignment.CenterVertically,
@@ -88,15 +117,15 @@ fun ProfilePage(context: ViewContext) {
             Spacer(modifier = Modifier.height(16.dp))
 
             // Option: Name with Edit button
-            OptionRow("Name", name) {
-                isEditingName = true
+            OptionRow("FirstName", firstName) {
+                profileViewModel.toggleEditFirstName(true)
             }
 
             Spacer(modifier = Modifier.height(8.dp))
 
             // Option: Email with Edit button
-            OptionRow("Email", email) {
-                isEditingEmail = true
+            OptionRow("LastName", lastName) {
+                profileViewModel.toggleEditLastName(true)
             }
 
             Spacer(modifier = Modifier.height(16.dp))
@@ -111,18 +140,14 @@ fun ProfilePage(context: ViewContext) {
                 // Nút "Save" với kiểu khác nhau tùy thuộc vào sự thay đổi đã được thực hiện hay không
                 Button(
                     onClick = {
-                        // Handle Save button click
-                        if (isChangesMade) {
-                            isEditingName = false
-                            isEditingEmail = false
-                            isChangesMade = false
-                            isUpdateSuccessfulDialogOpen = true
-                        }
+                        profileViewModel.saveProfile()
+                        isChangesMade = false
+                        Toast.makeText(currentContext, "Profile Saved", Toast.LENGTH_SHORT).show()
                     },
                     modifier = Modifier
                         .weight(1f)
                         .padding(end = 8.dp),
-                    enabled = isChangesMade // Kích hoạt/Ẩn nút tùy thuộc vào sự thay đổi
+                    enabled = isChangesMade
                 ) {
                     Text("Save")
                 }
@@ -141,38 +166,30 @@ fun ProfilePage(context: ViewContext) {
             }
 
         }
-        if (isUpdateSuccessfulDialogOpen) {
-            UpdateSuccessfulDialog {
-                // Xác nhận và đóng dialog
-                isUpdateSuccessfulDialogOpen = false
-                profileViewModel.updateName(newName)
-                profileViewModel.updateEmail(newEmail)
-            }
-        }
-        if (isEditingName) {
+        if (editFirstName) {
             EditNameDialog(
-                currentName = name,
+                currentName = firstName,
                 onNameChanged = { newName ->
-                    name = newName
+                    profileViewModel.onFirstNameChange(newName)
                     isChangesMade = true
-                    isEditingName = false
+                    profileViewModel.toggleEditFirstName(false)
                 },
                 onCancel = {
-                    isEditingName = false
+                    profileViewModel.toggleEditFirstName(false)
                 }
             )
         }
 
-        if (isEditingEmail) {
+        if (editLastName) {
             EditEmailDialog(
-                currentEmail = email,
-                onEmailChanged = { newEmail ->
-                    email = newEmail
+                currentEmail = lastName,
+                onEmailChanged = { newLastName ->
+                    profileViewModel.onLastNameChange(newLastName)
                     isChangesMade = true
-                    isEditingEmail = false
+                    profileViewModel.toggleEditLastName(false)
                 },
                 onCancel = {
-                    isEditingEmail = false
+                    profileViewModel.toggleEditLastName(false)
                 }
             )
         }
--- moon-fe/app/src/main/java/me/ppvan/moon/ui/viewmodel/ProfileViewModel.kt
+++ moon-fe/app/src/main/java/me/ppvan/moon/ui/viewmodel/ProfileViewModel.kt
@@ -1,25 +1,61 @@
 package me.ppvan.moon.ui.viewmodel
 
 //import me.ppvan.moon.data.repository.AuthService
-import androidx.compose.runtime.State
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import me.ppvan.moon.data.model.User
-import me.ppvan.moon.data.repository.ProfileRepository
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class ProfileViewModel @Inject constructor(private val profileRepository: ProfileRepository) : ViewModel() {
-    val loggedInUser: State<User> = profileRepository.loggedInUser
+class ProfileViewModel @Inject constructor() : ViewModel() {
+    private val _currentUser = MutableStateFlow(User.default())
+    val currentUser = _currentUser.asStateFlow()
 
-    // Các phương thức để cập nhật thông tin người dùng
-    fun updateName(newName: String) {
-        // Gọi phương thức updateName trong profileRepository
-        profileRepository.updateName(newName)
+    private val _firstName = MutableStateFlow("")
+    val firstName = _firstName.asStateFlow()
+
+    private val _lastName = MutableStateFlow("")
+    val lastName = _lastName.asStateFlow()
+
+    private val _avatarUrl = MutableStateFlow("")
+    val avatarUrl = _avatarUrl.asStateFlow()
+
+
+    private val _editFirstName = MutableStateFlow(false)
+    val editFirstName = _editFirstName.asStateFlow()
+
+    private val _editLastName = MutableStateFlow(false)
+    val editLastName = _editLastName.asStateFlow()
+
+    fun toggleEditFirstName(edit: Boolean) {
+        _editFirstName.update { edit }
     }
 
-    fun updateEmail(newEmail: String) {
-        // Gọi phương thức updateEmail trong profileRepository
-        profileRepository.updateEmail(newEmail)
+    fun toggleEditLastName(edit: Boolean) {
+        _editLastName.update { edit }
+    }
+
+
+    fun onFirstNameChange(newName: String) {
+        _firstName.update { newName }
+    }
+
+    fun onLastNameChange(newName: String) {
+        _lastName.update { newName }
+    }
+
+    fun onAvatarUrlChange(newUrl: String) {
+        _avatarUrl.update { newUrl }
+    }
+
+    fun saveProfile() {
+        Log.d("ProfileVM", "Saving profile")
+        Log.d("ProfileVM", firstName.value)
+        Log.d("ProfileVM", lastName.value)
+        _currentUser.update { it.copy(firstName = firstName.value, lastName = lastName.value, avatarUrl = avatarUrl.value) }
     }
 }
